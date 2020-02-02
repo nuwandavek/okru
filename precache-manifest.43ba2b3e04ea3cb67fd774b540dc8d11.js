40c25231972cdc045ceb15f32657c0a693565a49
nuwandavek <vivekaithal44@gmail.com>
2020-02-02 15:41:00 -0800

    added new domain

---
+++ precache-manifest.43ba2b3e04ea3cb67fd774b540dc8d11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f709b4c08249fb4ec657e2af575375",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c8b91109c00f8f76a92",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f9980fdbc141c859fe1d",
+    "url": "/static/js/2.27dbec02.chunk.js"
+  },
+  {
+    "revision": "9746fa97ffa7b41a2c7f52049b1f7870",
+    "url": "/static/js/2.27dbec02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c8b91109c00f8f76a92",
+    "url": "/static/js/main.dacae49c.chunk.js"
+  },
+  {
+    "revision": "72f68721f039ab8e9633",
+    "url": "/static/js/runtime-main.0ec733fe.js"
+  }
+]);
