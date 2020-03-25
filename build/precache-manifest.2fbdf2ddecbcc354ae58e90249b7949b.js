72e791a196a3589a00237903f357067af161a802
nuwandavek <vivekaithal44@gmail.com>
2020-03-25 01:07:39 -0700

    prod build of latest edits

---
+++ build/precache-manifest.2fbdf2ddecbcc354ae58e90249b7949b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b93f371915962fd0b0acc0a16d5ca98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc1e6acaac3b3541c7b6",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "25cce3699c0cc5c3f375",
+    "url": "/static/js/2.c3be855b.chunk.js"
+  },
+  {
+    "revision": "af3e829ba24ccd55d16ca5b6c1e2606f",
+    "url": "/static/js/2.c3be855b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc1e6acaac3b3541c7b6",
+    "url": "/static/js/main.b6f5ea8e.chunk.js"
+  },
+  {
+    "revision": "72f68721f039ab8e9633",
+    "url": "/static/js/runtime-main.0ec733fe.js"
+  }
+]);
