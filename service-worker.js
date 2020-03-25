b43ba48b389236ceb9bdbf7965148f2218b0c06f
nuwandavek <vivekaithal44@gmail.com>
2020-03-25 18:36:37 +0000

    Deploying to gh-pages from  - 8cc4bd6cd46264ee2920624b96a47aee93c84943 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2fbdf2ddecbcc354ae58e90249b7949b.js"
+  "/precache-manifest.6f09386bb8ad9b30d00425eb69ed8692.js"
 );
 
 self.addEventListener('message', (event) => {
