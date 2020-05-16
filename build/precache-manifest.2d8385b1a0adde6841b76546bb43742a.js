bae63dbe70240dd4d2e61a37f9a1421cbe7fdef2
nuwandavek <vivekaithal44@gmail.com>
2020-05-16 13:45:22 -0700

    build with refactor and copom

---
+++ build/precache-manifest.2d8385b1a0adde6841b76546bb43742a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8f91e482435c53f62185599ff588b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4928a4e97d720b26c65",
+    "url": "/static/css/main.2eee34f9.chunk.css"
+  },
+  {
+    "revision": "629a11c0c87bdebe68fa",
+    "url": "/static/js/2.f24ad8f3.chunk.js"
+  },
+  {
+    "revision": "513973645de0cf8bbd557639f7685fdd",
+    "url": "/static/js/2.f24ad8f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4928a4e97d720b26c65",
+    "url": "/static/js/main.19416625.chunk.js"
+  },
+  {
+    "revision": "e9fc2b29588cde37b2d6",
+    "url": "/static/js/runtime-main.58d26a38.js"
+  }
+]);
