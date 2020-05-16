8bcdad5fe15be387e69ed65272df70546ae87431
nuwandavek <vivekaithal44@gmail.com>
2020-05-16 21:59:04 +0000

    Deploying to gh-pages from  @ f2670c5b5472919334dfc2fc19db03e1f2e49495 🚀

---
+++ precache-manifest.c720f69dabe3cbb2777ec35bf5537cd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e20715022137d30421a78f8487cf7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48c4951c89adf5ecf309",
+    "url": "/static/css/main.2eee34f9.chunk.css"
+  },
+  {
+    "revision": "629a11c0c87bdebe68fa",
+    "url": "/static/js/2.f24ad8f3.chunk.js"
+  },
+  {
+    "revision": "513973645de0cf8bbd557639f7685fdd",
+    "url": "/static/js/2.f24ad8f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c4951c89adf5ecf309",
+    "url": "/static/js/main.d0c6a2ac.chunk.js"
+  },
+  {
+    "revision": "e9fc2b29588cde37b2d6",
+    "url": "/static/js/runtime-main.58d26a38.js"
+  }
+]);
